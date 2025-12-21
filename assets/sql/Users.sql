1f689d0f81fb60e7d91b8e5e924ab0de355722a4
kuroi-hime <kuroihime095@gmail.com>
2025-12-21 21:48:28 +0100

    sql: script d'insertion des utilisateurs.

--- assets/sql/Users.sql
+++ assets/sql/Users.sql
@@ -1,17 +1,17 @@
 -- Insertion des valeurs dans la table utilisateurs:
 -- Admin
 INSERT INTO utilisateurs (nom_user, email, role, motpasse_hash) VALUES
-('Admin Principal', 'admin@zoo.com', 'admin', SHA2('admin123', 256));
+('Admin Principal', 'admin@zoo.com', 'admin', md5('admin123'));
 
 -- Guides
 INSERT INTO utilisateurs (nom_user, email, role, motpasse_hash) VALUES
-('Asmae Lhamri', 'asmea.savane@zoo.com', 'guide', SHA2('guideAsmae123', 256)),
-('Khalid Ben Zida', 'benZida.tropiques@zoo.com', 'guide', SHA2('guideKhalid123', 256)),
-('Omar Nabil', 'o.nabil.nocturne@zoo.com', 'guide', SHA2('guideOmar123', 256));
+('Asmae Lhamri', 'asmea.savane@zoo.com', 'guide', md5('guideAsmae123')),
+('Khalid Ben Zida', 'benZida.tropiques@zoo.com', 'guide', md5('guideKhalid123')),
+('Omar Nabil', 'o.nabil.nocturne@zoo.com', 'guide', md5('guideOmar123'));
 
 -- Visiteurs
 INSERT INTO utilisateurs (nom_user, email, role, motpasse_hash) VALUES
-('Youssef El Amrani', 'youssef.elamrani@zoo.com', 'visiteur', SHA2('Y0uss3f!2025', 256)),
-('Sara Benali', 'sara.benali@zoo.com', 'visiteur', SHA2('S@ra#Zoo2025', 256)),
-('Hassan Idrissi', 'hassan.idrissi@zoo.com', 'visiteur', SHA2('Hass@n_1234', 256)),
-('Meryem Fassi', 'meryem.fassi@zoo.com', 'visiteur', SHA2('M3ryem*Zoo', 256));
+('Youssef El Amrani', 'youssef.elamrani@zoo.com', 'visiteur', md5('Y0uss3f!2025')),
+('Sara Benali', 'sara.benali@zoo.com', 'visiteur', md5('S@ra#Zoo2025')),
+('Hassan Idrissi', 'hassan.idrissi@zoo.com', 'visiteur', md5('Hass@n_1234')),
+('Meryem Fassi', 'meryem.fassi@zoo.com', 'visiteur', md5('M3ryem*Zoo'));
