b909e37b182ddbb332a2bbb55222283317270720
kuroi-hime <kuroihime095@gmail.com>
2025-12-21 21:49:47 +0100

    php: établissement de connexion avec BD.

---
+++ assets/php/connexionBD.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost";
+    $user = "root";
+    $password = "";
+    $dbName   = "ASSAD";
+    $connexion = new mysqli($host, $user, $password, $dbName) or die('Connexion à échouée');
+?>
